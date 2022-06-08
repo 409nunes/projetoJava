f3e0244fb21fd9077366c34cacf379320c760747
@409nunes <eduardosky2011@hotmail.com>
2022-06-08 20:15:33 -0300

    java alterado

---
+++ exercici/CaixaTeste.java
@@ -0,0 +1,23 @@
+package exercici;
+
+class CaixaTeste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Caixa<String> caixaA = new Caixa<>();
+		caixaA.guardar("Segredo!");
+		
+		String coisaA = caixaA.abrir();
+		System.out.println(coisaA);
+		
+		Caixa<String> caixaB = new Caixa<>();
+		caixaB.guardar("Segredo!");
+		
+		String coisaB = caixaB.abrir();
+		System.out.println(coisaB);
+		
+		System.out.println("Ola mundo");
+	}
+
+}
